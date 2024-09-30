ee14abba3f9d9480409d75c1224bc644aa26435d
Armemius <rusgy@yandex.ru>
2024-09-30 10:12:13 +0300

    improve prob28 solution

--- lab-1/src/prob28.rs
+++ lab-1/src/prob28.rs
@@ -1,6 +1,7 @@
 pub(crate) fn solution(n: u64) -> u64 {
-    (1..(n - 1) / 2 + 1)
-        .flat_map(|x| std::iter::repeat(x * 2).take(4))
+    (2..=n)
+        .step_by(2)
+        .flat_map(|x| std::iter::repeat(x).take(4))
         .scan(1, |acc, curr| {
             *acc += curr;
             Some(*acc)
