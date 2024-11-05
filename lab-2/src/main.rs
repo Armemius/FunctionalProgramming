33f839c31220bf236f4ec402a5b9d7766638d1f0
Armemius <rusgy@yandex.ru>
2024-11-05 20:39:28 +0300

    implemented rotation

--- lab-2/src/main.rs
+++ lab-2/src/main.rs
@@ -3,15 +3,12 @@ use node::Tree;
 mod node;
 
 fn main() {
-    let mut tree = Tree::<i32, &str>::new();
+    let mut tree = Tree::<i32, i32>::new();
 
-    tree.insert(8, "a");
-    tree.insert(4, "b");
-    tree.insert(12, "c");
-    tree.insert(2, "d");
-    tree.insert(6, "e");
-    tree.insert(10, "f");
-    tree.insert(14, "g");
+    for it in (0..128).rev() {
+        tree.insert(it, it);
+    }
+    // tree.insert(14, "g");
 
     tree.dump_tree();
 }
--- lab-2/src/node.rs
+++ lab-2/src/node.rs
@@ -24,6 +24,14 @@ where
     parent: Option<WeakNodeRef<K, V>>,
 }
 
+impl<K, V> PartialEq for Node<K, V>
+where
+    K: Ord, {
+    fn eq(&self, other: &Self) -> bool {
+        self.key == other.key
+    }
+}
+
 impl<K, V> Node<K, V>
 where
     K: Ord,
@@ -66,29 +74,136 @@ where
         Self { root: None }
     }
 
-    fn balance(&mut self, node: NodeRef<K, V>) {
-        // todo!();
+    fn balance_insertion(&mut self, node: NodeRef<K, V>) {
+        let parent_option = {
+            let node_ref = node.borrow();
+            node_ref.parent.clone()
+        };
+        if parent_option.is_none() {
+            node.borrow_mut().color = Color::Black;
+            return;
+        }
+
+        let parent = parent_option.unwrap().upgrade();
+        let uncle = Self::get_uncle(node.clone());
+        let grandparent = Self::get_grandparent(node.clone());
+
+        let parent_color = Self::get_node_color(parent.clone());
+        let uncle_color = Self::get_node_color(uncle.clone());
+
+        match (parent_color, uncle_color) {
+            (Color::Black, _) => {
+                dbg!("Case 0: Black parent, no need to balance");
+            },
+            (_, Color::Red) => {
+                let parent_rc = parent.unwrap();
+                {
+                    let mut parent_ref = parent_rc.borrow_mut();
+                    parent_ref.color = Color::Black;
+                }
+                let uncle_rc = uncle.unwrap();
+                {
+                    let mut uncle_ref = uncle_rc.borrow_mut();
+                    uncle_ref.color = Color::Black;
+                }
+                let grandparent_rc = grandparent.unwrap();
+                {
+                    let mut grandparent_ref = grandparent_rc.borrow_mut();
+                    grandparent_ref.color = Color::Red;
+                }
+                dbg!("Case 1: Recoloring parent, uncle, and grandparent");
+                self.balance_insertion(grandparent_rc);
+            }
+            (_, Color::Black) => {
+                let parent_rc = parent.unwrap();
+                let grandparent_rc = grandparent.unwrap();
+                let is_left_child = {
+                    let grandparent_ref = grandparent_rc.borrow();
+                    if let Some(left_child) = &grandparent_ref.left {
+                        Rc::ptr_eq(&parent_rc, left_child)
+                    } else {
+                        false
+                    }
+                };
+                if is_left_child {
+                    let node_is_right_child = {
+                        let parent_ref = parent_rc.borrow();
+                        if let Some(right_child) = &parent_ref.right {
+                            Rc::ptr_eq(&node, right_child)
+                        } else {
+                            false
+                        }
+                    };
+                    if node_is_right_child {
+                        self.rotate_left(parent_rc.clone());
+                        dbg!("Case 2: Left rotate");
+                        self.balance_insertion(parent_rc);
+                    } else {
+                        {
+                            let mut grandparent_ref = grandparent_rc.borrow_mut();
+                            grandparent_ref.color = Color::Red;
+                        }
+                        {
+                            let mut parent_ref = parent_rc.borrow_mut();
+                            parent_ref.color = Color::Black;
+                        }
+                        self.rotate_right(grandparent_rc);
+                        dbg!("Case 3: Right rotate");
+                    }
+                } else {
+                    let node_is_left_child = {
+                        let parent_ref = parent_rc.borrow();
+                        if let Some(left_child) = &parent_ref.left {
+                            Rc::ptr_eq(&node, left_child)
+                        } else {
+                            false
+                        }
+                    };
+                    if node_is_left_child {
+                        self.rotate_right(parent_rc.clone());
+                        dbg!("Case 2: Right rotate");
+                        self.balance_insertion(parent_rc);
+                    } else {
+                        {
+                            let mut grandparent_ref = grandparent_rc.borrow_mut();
+                            grandparent_ref.color = Color::Red;
+                        }
+                        {
+                            let mut parent_ref = parent_rc.borrow_mut();
+                            parent_ref.color = Color::Black;
+                        }
+                        self.rotate_left(grandparent_rc);
+                        dbg!("Case 3: Left rotate");
+                    }
+                }
+            }
+            _ => {
+                dbg!((parent_color, uncle_color));
+            }
+        }
     }
 
     fn insert_helper(&self, current: NodeRef<K, V>, key: K, value: V) -> Option<NodeRef<K, V>> {
         let mut current_node = current.borrow_mut();
         if current_node.key > key {
             if let Some(left) = current_node.left.clone() {
+                drop(current_node);
                 self.insert_helper(left, key, value)
             } else {
                 let new_node = Rc::new(RefCell::new(Node::new_red(key, value)));
                 current_node.left = Some(new_node.clone());
                 new_node.borrow_mut().parent = Some(Rc::downgrade(&current));
-                Some(new_node.clone())
+                Some(new_node)
             }
         } else if current_node.key < key {
             if let Some(right) = current_node.right.clone() {
+                drop(current_node);
                 self.insert_helper(right, key, value)
             } else {
                 let new_node = Rc::new(RefCell::new(Node::new_red(key, value)));
                 current_node.right = Some(new_node.clone());
                 new_node.borrow_mut().parent = Some(Rc::downgrade(&current));
-                Some(new_node.clone())
+                Some(new_node)
             }
         } else {
             current_node.value = value;
@@ -100,16 +215,213 @@ where
         if let Some(root) = self.root.clone() {
             let new_node = self.insert_helper(root, key, value);
             if let Some(node) = new_node {
-                self.balance(node);
+                self.balance_insertion(node);
             }
         } else {
             self.root = Some(Rc::new(RefCell::new(Node::new_black(key, value))));
         }
     }
+
+    fn get_node_color(node: Option<NodeRef<K, V>>) -> Color {
+        if let Some(node) = node {
+            node.borrow().color
+        } else {
+            Color::Black
+        }
+    }
+    
+    fn get_grandparent(node: Rc<RefCell<Node<K, V>>>) -> Option<NodeRef<K, V>> {
+        let parent_weak = {
+            let node_ref = node.borrow();
+            node_ref.parent.clone()
+        };
+        let parent_rc = parent_weak?.upgrade()?;
+        let grandparent_weak = {
+            let parent_ref = parent_rc.borrow();
+            parent_ref.parent.clone()
+        };
+        grandparent_weak?.upgrade()
+    }
+
+    fn get_uncle(node: Rc<RefCell<Node<K, V>>>) -> Option<NodeRef<K, V>> {
+        let parent_weak = {
+            let node_ref = node.borrow();
+            node_ref.parent.clone()
+        };
+        let parent_rc = parent_weak?.upgrade()?;
+        let grandparent_weak = {
+            let parent_ref = parent_rc.borrow();
+            parent_ref.parent.clone()
+        };
+        let grandparent_rc = grandparent_weak?.upgrade()?;
+
+        let parent_is_left_child = {
+            let grandparent_ref = grandparent_rc.borrow();
+            if let Some(left_child) = &grandparent_ref.left {
+                Rc::ptr_eq(&parent_rc, left_child)
+            } else {
+                false
+            }
+        };
+
+        if parent_is_left_child {
+            let grandparent_ref = grandparent_rc.borrow();
+            grandparent_ref.right.clone()
+        } else {
+            let grandparent_ref = grandparent_rc.borrow();
+            grandparent_ref.left.clone()
+        }
+    }
+    
+    fn rotate_left(&mut self, x: Rc<RefCell<Node<K, V>>>) {
+        let y_option = {
+            let x_ref = x.borrow();
+            x_ref.right.clone()
+        };
+        let y = match y_option {
+            Some(node) => node,
+            None => return, // Cannot rotate left if x.right is None
+        };
+
+        // Begin rotation
+        {
+            // Move y.left to x.right
+            let mut x_mut = x.borrow_mut();
+            x_mut.right = {
+                let y_ref = y.borrow();
+                y_ref.left.clone()
+            };
+        }
+
+        // Update y.left.parent to x if y.left is not None
+        if let Some(ref x_right) = x.borrow().right {
+            x_right.borrow_mut().parent = Some(Rc::downgrade(&x));
+        }
+
+        // Update y.parent to x.parent
+        {
+            let x_parent_option = {
+                let x_ref = x.borrow();
+                x_ref.parent.clone()
+            };
+            y.borrow_mut().parent = x_parent_option.clone();
+        }
+
+        // Update x.parent's child pointer to y
+        {
+            let x_parent_option = {
+                let x_ref = x.borrow();
+                x_ref.parent.clone()
+            };
+
+            if let Some(x_parent_weak) = x_parent_option {
+                if let Some(x_parent) = x_parent_weak.upgrade() {
+                    let mut x_parent_mut = x_parent.borrow_mut();
+                    if let Some(ref left_child) = x_parent_mut.left {
+                        if Rc::ptr_eq(&x, left_child) {
+                            x_parent_mut.left = Some(y.clone());
+                        }
+                    }
+                    if let Some(ref right_child) = x_parent_mut.right {
+                        if Rc::ptr_eq(&x, right_child) {
+                            x_parent_mut.right = Some(y.clone());
+                        }
+                    }
+                }
+            } else {
+                // x was root, update self.root to y
+                self.root = Some(y.clone());
+            }
+        }
+
+        // Set y.left = x
+        {
+            let mut y_mut = y.borrow_mut();
+            y_mut.left = Some(x.clone());
+        }
+
+        // Update x.parent to y
+        {
+            x.borrow_mut().parent = Some(Rc::downgrade(&y));
+        }
+    }
+
+    fn rotate_right(&mut self, x: Rc<RefCell<Node<K, V>>>) {
+        let y_option = {
+            let x_ref = x.borrow();
+            x_ref.left.clone()
+        };
+        let y = match y_option {
+            Some(node) => node,
+            None => return, // Cannot rotate right if x.left is None
+        };
+
+        // Begin rotation
+        {
+            // Move y.right to x.left
+            let mut x_mut = x.borrow_mut();
+            x_mut.left = {
+                let y_ref = y.borrow();
+                y_ref.right.clone()
+            };
+        }
+
+        // Update y.right.parent to x if y.right is not None
+        if let Some(ref x_left) = x.borrow().left {
+            x_left.borrow_mut().parent = Some(Rc::downgrade(&x));
+        }
+
+        // Update y.parent to x.parent
+        {
+            let x_parent_option = {
+                let x_ref = x.borrow();
+                x_ref.parent.clone()
+            };
+            y.borrow_mut().parent = x_parent_option.clone();
+        }
+
+        // Update x.parent's child pointer to y
+        {
+            let x_parent_option = {
+                let x_ref = x.borrow();
+                x_ref.parent.clone()
+            };
+
+            if let Some(x_parent_weak) = x_parent_option {
+                if let Some(x_parent) = x_parent_weak.upgrade() {
+                    let mut x_parent_mut = x_parent.borrow_mut();
+                    if let Some(ref left_child) = x_parent_mut.left {
+                        if Rc::ptr_eq(&x, left_child) {
+                            x_parent_mut.left = Some(y.clone());
+                        }
+                    }
+                    if let Some(ref right_child) = x_parent_mut.right {
+                        if Rc::ptr_eq(&x, right_child) {
+                            x_parent_mut.right = Some(y.clone());
+                        }
+                    }
+                }
+            } else {
+                // x was root, update self.root to y
+                self.root = Some(y.clone());
+            }
+        }
+
+        // Set y.right = x
+        {
+            let mut y_mut = y.borrow_mut();
+            y_mut.right = Some(x.clone());
+        }
+
+        // Update x.parent to y
+        {
+            x.borrow_mut().parent = Some(Rc::downgrade(&y));
+        }
+    }
 }
 
 
-// Ouput 
+// Output
 
 impl<K, V> Tree<K, V>
 where
