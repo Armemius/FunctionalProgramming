c5f6f8a3b2d3761aabdb1c3bee8b959eacc174a8
Armemius <rusgy@yandex.ru>
2024-11-24 21:26:28 +0300

    gh-pages

--- lab-1-cpp/prob28/include/prob28.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-auto spiral_diagonals_sum(uint64_t size) -> uint64_t;
--- lab-1-cpp/prob3/include/prob3.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-auto largest_prime_factor(uint64_t n) -> uint64_t;
