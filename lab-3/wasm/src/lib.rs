cb35e2f533233acd57737e8486fa3f0b5ad4a2f0
Armemius <rusgy@yandex.ru>
2024-12-10 20:00:27 +0300

    [~] Separate interop & make calc frame & split state

--- lab-3/wasm/src/lib.rs
+++ lab-3/wasm/src/lib.rs
@@ -5,6 +5,8 @@ use serde::{Deserialize, Serialize};
 use serde_wasm_bindgen::{from_value, to_value};
 use wasm_bindgen::prelude::*;
 
+const MAX_POINTS: usize = 10;
+
 fn check_point_order(points: &Vec<Point>) -> bool {
     points.windows(2).all(|window| match window {
         [p1, p2] => p1.x < p2.x,
@@ -31,7 +33,9 @@ pub fn parse_points(input: String) -> Result<JsValue, JsError> {
         return Err(JsError::new(&format!("{}", err)));
     }
 
-    let data: Vec<Point> = points.map(|point| point.unwrap()).collect();
+    let data: Vec<Point> = points
+        .map(|point| point.unwrap())
+        .collect::<Vec<Point>>();
     to_value(&data).map_err(|err| JsError::new(&format!("{}", err)))
 }
 
@@ -47,6 +51,28 @@ struct InterpolationResult {
     method: String,
     latex_equation: String,
     points: Vec<Point>,
+}
+
+fn process_data(
+    methods: &Vec<String>,
+    points: &Vec<Point>,
+    step: f64,
+) -> Result<Vec<InterpolationResult>, String> {
+    methods
+        .iter()
+        .filter_map(|method| {
+            let interpolator = interpolator::get_interpolator(method).unwrap();
+            if interpolator.points_required().start <= points.len() {
+                Some(Ok(InterpolationResult {
+                    method: method.clone(),
+                    latex_equation: interpolator.latex_equation(&points),
+                    points: interpolator.interpolate_points(&points, step),
+                }))
+            } else {
+                None
+            }
+        })
+        .collect::<Result<Vec<InterpolationResult>, String>>()
 }
 
 #[wasm_bindgen]
@@ -68,28 +94,20 @@ pub fn process(data: &JsValue) -> Result<JsValue, JsError> {
         return Err(JsError::new("Points must be ordered by x increasing"));
     }
 
-    let res = data
-        .methods
-        .iter()
-        .filter_map(|method| {
-            let interpolator = interpolator::get_interpolator(method).unwrap();
-            if interpolator.points_required().start <= data.points.len() {
-                Some(Ok(InterpolationResult {
-                    method: method.clone(),
-                    latex_equation: interpolator.latex_equation(&data.points),
-                    points: interpolator.interpolate_points(&data.points, data.step),
-                }))
-            } else {
-                None
-            }
-        })
-        .collect::<Result<Vec<InterpolationResult>, JsError>>()
-        .map(|results| {
-            to_value(&results).map_err(|err| JsError::new(&format!("{}", err)))
-        });
+    let res = process_data(
+        &data.methods,
+        &data.points[(if data.points.len() < MAX_POINTS {
+            0
+        } else {
+            data.points.len() - MAX_POINTS
+        })..]
+            .to_vec(),
+        data.step,
+    )
+    .map(|results| to_value(&results).map_err(|err| JsError::new(&format!("{}", err))));
 
     match res {
         Ok(res) => res,
-        Err(err) => Err(err),
+        Err(err) => Err(JsError::new(&format!("{}", err))),
     }
 }
