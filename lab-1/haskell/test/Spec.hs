3ccd342219fcd43a7b649610e5372e131f1f22c5
Armemius <rusgy@yandex.ru>
2025-09-20 18:21:14 +0300

    test(lab-1): test prob5 solutions
    Add unit tests for implemented solutions using Tasty testing framework

--- lab-1/haskell/test/Spec.hs
+++ lab-1/haskell/test/Spec.hs
@@ -1,36 +1,31 @@
-import Test.Tasty
-import Test.Tasty.HUnit
-import Test.Tasty.QuickCheck as QC
-
-addTwo :: Int -> Int -> Int
-addTwo x y = x + y
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Prob5
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "All tests"
-  [ unitTests
-  , propertyTests
-  ]
-
-unitTests :: TestTree
-unitTests = testGroup "Unit tests"
-  [ testCase "2 + 3 = 5" $
-      addTwo 2 3 @?= 5
-
-  , testCase "0 + 0 = 0" $
-      addTwo 0 0 @?= 0
-  ]
-
-propertyTests :: TestTree
-propertyTests = testGroup "Property tests"
-  [ QC.testProperty "addition is commutative" $
-      \x y -> addTwo x y == addTwo y x
-
-  , QC.testProperty "addition with 0 keeps number" $
-      \x -> addTwo x 0 == (x :: Int)
-
-  , QC.testProperty "addition is associative" $
-      \x y z -> addTwo x (addTwo y z) == addTwo (addTwo x y) z
+tests = testGroup
+  "Problem 5 variants"
+  [ testCase "recProb5 10" $ recProb5 10 @?= 2520
+  , testCase "tailRecProb5 10" $ tailRecProb5 10 @?= 2520
+  , testCase "mapProb5 10" $ mapProb5 10 @?= 2520
+  , testCase "infiniteListProb5 10" $ infiniteListProb5 10 @?= 2520
+  , testCase "moduleProb5 10" $ moduleProb5 10 @?= 2520
+  , testCase "recProb5 20" $ recProb5 20 @?= 232792560
+  , testCase "tailRecProb5 20" $ tailRecProb5 20 @?= 232792560
+  , testCase "mapProb5 20" $ mapProb5 20 @?= 232792560
+  , testCase "infiniteListProb5 20" $ infiniteListProb5 20 @?= 232792560
+  , testCase "moduleProb5 20" $ moduleProb5 20 @?= 232792560
+  , testCase "All variants (n=10) equal"
+    $ let n = 10
+          vals =
+            [tailRecProb5 n, mapProb5 n, infiniteListProb5 n, moduleProb5 n]
+      in  all (== recProb5 n) vals @? "Mismatch between implementations"
+  , testCase "All variants (n=20) equal"
+    $ let n = 20
+          vals =
+            [tailRecProb5 n, mapProb5 n, infiniteListProb5 n, moduleProb5 n]
+      in  all (== recProb5 n) vals @? "Mismatch between implementations"
   ]
